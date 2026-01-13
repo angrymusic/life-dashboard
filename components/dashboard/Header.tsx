7748db4ebf767bef6822e009821c444d50b70248
이민재 <angrymusic@naver.com>
2026-01-13 22:05:03 +0900

    style: button styling border 보이게

--- components/dashboard/Header.tsx
+++ components/dashboard/Header.tsx
@@ -8,18 +8,18 @@ export default function Header() {
         variant="outline"
         size="icon-lg"
         aria-label="Menu"
-        className="rounded-full bg-white/60 backdrop-blur-[2px] border-white/40 hover:bg-white/80 pointer-events-auto"
+        className="rounded-full bg-white/60 backdrop-blur-[2px] hover:bg-white/80 pointer-events-auto"
       >
         <Menu className="h-5 w-5" />
       </Button>
-      <div className="border bg-white/60 backdrop-blur-[2px] border-white/40 hover:bg-white/80 shadow-sm px-6 py-2 rounded-full font-medium text-sm pointer-events-auto">
-        Dashboard
+      <div className="border bg-white/60 backdrop-blur-[2px] hover:bg-white/80 shadow-sm px-6 py-2 rounded-full font-medium text-sm pointer-events-auto">
+        New Dashboard
       </div>
       <Button
         variant="outline"
         size="icon-lg"
         aria-label="Menu"
-        className="rounded-full bg-white/60 backdrop-blur-[2px] border-white/40 hover:bg-white/80 pointer-events-auto"
+        className="rounded-full bg-white/60 backdrop-blur-[2px] hover:bg-white/80 pointer-events-auto"
       >
         <Menu className="h-5 w-5" />
       </Button>
